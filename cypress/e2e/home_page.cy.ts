dc6a36be6548c47144a18057370498e03238d837
simonsafhalter <simon.safhalter@masabi.com>
2024-02-06 00:19:09 +0100

    Update Cypress test.

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,9 +1,17 @@
-describe('The Home Page', () => {
-    it('successfully loads', () => {
+describe('Image Editor', () => {
+    it('Browse, select, edit and download an image', () => {
         cy.visit('/')
 
-        cy.findByRole('button', {
-            name: /Next Page/i,
-        }).click()
+        // Click the next page button
+        cy.get('button[aria-label="Next Page"]').click()
+
+        // Click on the 2nd element in the image list
+        cy.get('ul>li').eq(2).click()
+
+        // Edit the image to grayscale
+        cy.get('input[type="checkbox"]').click()
+
+        // Download the image
+        cy.contains('button', 'Download image').click()
     })
 })
